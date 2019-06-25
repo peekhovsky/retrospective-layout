b43b4d6dfdff2d09d2c2356da351b9f7ec362ced
peekhovsky@gmail.com <stevegates1>
2019-06-25 17:38:14 +0300

    upd

--- src/main.js
+++ src/main.js
@@ -7,9 +7,11 @@ $(document).ready(() => {
     $('#upside-block').hover(() => {
 
     })
-    $('#left-block').hover(() => {
-        $('#left-block').css({'z-index': '10'})
-    }, () => {
+    $('#left-block').hover(
+        () => {
+            $('#left-block').css({'z-index': '10'})
+        }, 
+        () => {
         setTimeout(() => {
             $('#left-block').css({'z-index': '4'})
         }, 100);
