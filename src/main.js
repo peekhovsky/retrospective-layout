3a2771ceffdba0e8236db1aa015fbddf75b26085
Rastsislau Piakhouski <rastsislau_piakhouski@epam.com>
2019-06-28 02:05:29 +0300

    v3

--- src/main.js
+++ src/main.js
@@ -2,18 +2,31 @@ let $ = require('jquery');
 
 let openedTab = 'center';
 
-
 $(document).ready(() => {
-    $('#upside-block').hover(() => {
-
-    })
+   $('#video').play();
     $('#left-block').hover(
         () => {
-            $('#left-block').css({'z-index': '10'})
-        }, 
+            $('#left-block').css({ 'z-index': '10' })
+        },
         () => {
-        setTimeout(() => {
-            $('#left-block').css({'z-index': '4'})
-        }, 100);
-    })
+            setTimeout(() => {
+                $('#left-block').css({ 'z-index': '4' })
+            }, 100);
+        })
 });
+
+function msToTime(ms) {
+    let seconds = (ms / 1000);
+    let minutes = parseInt(seconds / 60, 10);
+    seconds = Math.floor(seconds % 60);
+    let hours = parseInt(minutes / 60, 10);
+    minutes = minutes % 60;
+    return hours + 'h:' + minutes + 'm:' + seconds + 's';
+}
+
+let eventDate = new Date(2020, 3, 19, 16, 30, 0, 0);
+
+setInterval(() => {
+    let currentDate = msToTime(eventDate - new Date());
+    $('#timer').text(currentDate);
+}, 1000)
